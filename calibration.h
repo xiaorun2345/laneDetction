5f3aed996542c4eb2ab33ed7ca7a0b16bc8618d6
xiaorun2345 <15610499173@163.com>
2021-02-26 12:28:31 +0800

    lanedetection

---
+++ calibration.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <string>
+
+using namespace cv;
+using namespace std;
+
+class CameraCalibrator
+{
+private:
+	vector<string> m_filenames;
+	vector<vector<Point2f> > m_srcPoints;
+	vector<vector<Point3f> > m_dstPoints;
+	Size m_imageSize;
+public:
+	void setFilename();
+	void addPoints();
+	void doCalibration(Mat& cameraMatrix, Mat& dist);
+};
